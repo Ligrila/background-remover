3a4857c53ac60e1a9c78dda205b93b921e2b6a0f
Leandro Emanuel López <lopezlean@gmail.com>
2025-07-15 22:03:15 +0200

    those members must have an 'override'

--- src/loading-spinner.ts
+++ src/loading-spinner.ts
@@ -1,5 +1,4 @@
-
-import { LitElement, html, unsafeCSS } from 'lit';
+import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 
@@ -8,17 +7,17 @@ import { loadingSpinnerStyles } from './loading-spinner.styles';
 
 @customElement('loading-spinner')
 export class LoadingSpinner extends LitElement {
-  static styles = [loadingSpinnerStyles];
+  static override styles = [loadingSpinnerStyles];
 
   private renderSparkles() {
     const sparkles = Array.from(
       { length: 20 },
-      () => html`<div class="sparkle">${unsafeHTML(sparkleIcon)}</div>`,
+      () => html`<div class="sparkle">${unsafeHTML(sparkleIcon)}</div>`
     );
     return html`${sparkles}`;
   }
 
-  render() {
+  override render() {
     return html`${this.renderSparkles()}`;
   }
 }
